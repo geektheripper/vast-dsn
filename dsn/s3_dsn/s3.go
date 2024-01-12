34e82e3ed5867724cafbee373d5102a7d54b2143
GeekTR <geektheripper@gmail.com>
2024-01-12 12:29:47 +0800

    update s3 dsn with bucket and object definition

--- dsn/s3_dsn/s3.go
+++ dsn/s3_dsn/s3.go
@@ -2,13 +2,15 @@ package s3_dsn
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
-func ParseDSN(dsn string) (config *aws.Config, err error) {
+func ParseDSN(dsn string) (config *aws.Config, bucket string, key string, err error) {
 	config = &aws.Config{}
 
 	url, err := url.Parse(dsn)
@@ -54,14 +56,78 @@ func ParseDSN(dsn string) (config *aws.Config, err error) {
 	config.DisableSSL = aws.Bool(disableSsl)
 	config.S3ForcePathStyle = aws.Bool(s3ForcePathStyle)
 
+	path := url.Path
+	if path != "" {
+		parts := strings.Split(strings.TrimLeft(path, "/"), "/")
+		bucket = parts[0]
+		key = strings.Join(parts[1:], "/")
+	}
+
 	return
 }
 
-func MustParseDSN(dsn string) *aws.Config {
-	config, err := ParseDSN(dsn)
+func MustParseDSN(dsn string) (config *aws.Config, bucket string, key string, err error) {
+	config, bucket, key, err = ParseDSN(dsn)
 	if err != nil {
 		panic(err)
 	}
+	return
+}
 
-	return config
+func ParseS3DSN(dsn string) (config *aws.Config, err error) {
+	config, bucket, _, err := ParseDSN(dsn)
+	if bucket != "" {
+		return nil, fmt.Errorf("invalid s3 dsn: unexpected bucket: %s", bucket)
+	}
+
+	return
+}
+
+func MustParseS3DSN(dsn string) (config *aws.Config, err error) {
+	config, err = ParseS3DSN(dsn)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func ParseS3BucketDSN(dsn string) (config *aws.Config, bucket string, err error) {
+	config, bucket, key, err := ParseDSN(dsn)
+
+	if bucket == "" {
+		return nil, "", fmt.Errorf("invalid s3 bucket dsn: missing bucket")
+	}
+
+	if key != "" {
+		return nil, "", fmt.Errorf("invalid s3 bucket dsn: unexpected key: %s", key)
+	}
+
+	return
+}
+
+func MustParseS3BucketDSN(dsn string) (config *aws.Config, bucket string, err error) {
+	config, bucket, err = ParseS3BucketDSN(dsn)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+func ParseS3ObjectDSN(dsn string) (config *aws.Config, bucket string, key string, err error) {
+	config, bucket, key, err = ParseDSN(dsn)
+	if bucket == "" {
+		return nil, "", "", fmt.Errorf("invalid s3 object dsn: missing bucket")
+	}
+	if key == "" {
+		return nil, "", "", fmt.Errorf("invalid s3 object dsn: missing key")
+	}
+	return
+}
+
+func MustParseS3ObjectDSN(dsn string) (config *aws.Config, bucket string, key string, err error) {
+	config, bucket, key, err = ParseS3ObjectDSN(dsn)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
--- dsn/s3_dsn/s3_test.go
+++ dsn/s3_dsn/s3_test.go
@@ -1,6 +1,7 @@
 package s3_dsn_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/geektheripper/vast-dsn/dsn/s3_dsn"
@@ -8,7 +9,7 @@ import (
 
 func TestS3DSNParser(t *testing.T) {
 	t.Run("amazon s3", func(t *testing.T) {
-		config, err := s3_dsn.ParseDSN("s3://access_key:secret_key@-?region=us-east-2")
+		config, err := s3_dsn.ParseS3DSN("s3://access_key:secret_key@-?region=us-east-2")
 		if err != nil {
 			t.Error(err)
 			return
@@ -32,7 +33,7 @@ func TestS3DSNParser(t *testing.T) {
 	})
 
 	t.Run("self sign https minio", func(t *testing.T) {
-		config, err := s3_dsn.ParseDSN("s3://access_key:secret_key@maggie.minio.geektr.co:9003?region=")
+		config, err := s3_dsn.ParseS3DSN("s3://access_key:secret_key@maggie.minio.geektr.co:9003?region=")
 		if err != nil {
 			t.Error(err)
 			return
@@ -56,6 +57,39 @@ func TestS3DSNParser(t *testing.T) {
 
 		if *config.Endpoint != "https://maggie.minio.geektr.co:9003" {
 			t.Error("endpoint not parsed")
+			return
+		}
+	})
+
+	t.Run("s3 bucket dsn", func(t *testing.T) {
+		config, bucket, err := s3_dsn.ParseS3BucketDSN("s3://access_key:secret_key@maggie.minio.geektr.co:9003/foobar/path/to/key?region=")
+		if bucket != "" || config != nil {
+			t.Error("wrong bucket dsn parsed")
+			return
+		}
+
+		if fmt.Sprint(err) != fmt.Sprintf("invalid s3 bucket dsn: unexpected key: %s", "path/to/key") {
+			t.Error(err)
+			return
+		}
+
+		_, bucket2, _ := s3_dsn.ParseS3BucketDSN("s3://access_key:secret_key@maggie.minio.geektr.co:9003/foobar?region=")
+
+		if bucket2 != "foobar" {
+			t.Error("bucket not parsed")
+			return
+		}
+	})
+
+	t.Run("s3 object dsn", func(t *testing.T) {
+		_, _, key, err := s3_dsn.ParseS3ObjectDSN("s3://access_key:secret_key@maggie.minio.geektr.co:9003/foobar/path/to/key?region=")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if key != "path/to/key" {
+			t.Error("key not parsed")
 			return
 		}
 	})
