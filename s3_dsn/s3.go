ace400fcf5349ce6ee1d728b5714d55aec965553
GeekTR <geektheripper@gmail.com>
2025-03-09 13:47:26 +0800

    add logger for MustParse

---
+++ s3_dsn/s3.go
@@ -0,0 +1,142 @@
+package s3_dsn
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/geektheripper/vast-dsn/utils"
+)
+
+func Parse(dsn string) (config *aws.Config, bucket string, key string, err error) {
+	config = &aws.Config{}
+
+	url, err := url.Parse(dsn)
+	if err != nil {
+		return
+	}
+
+	if url.Scheme != "s3" {
+		err = errors.New("invalid scheme")
+		return
+	}
+
+	user := url.User.Username()
+	pass, passOk := url.User.Password()
+
+	protocol := "https"
+	if url.Query().Has("protocol") {
+		protocol = url.Query().Get("protocol")
+		if protocol != "http" && protocol != "https" {
+			err = errors.New("invalid protocol")
+			return
+		}
+	}
+
+	region := "us-east-1"
+	if url.Query().Has("region") {
+		region = url.Query().Get("region")
+	}
+
+	disableSsl := url.Query().Get("disable-ssl") == "true"
+	s3ForcePathStyle := url.Query().Get("s3-force-path-style") == "true"
+
+	if user != "" && passOk {
+		config.Credentials = credentials.NewStaticCredentials(user, pass, "")
+	}
+
+	endpoint := url.Host
+	if endpoint != "-" {
+		config.Endpoint = aws.String(protocol + "://" + endpoint)
+	}
+
+	config.Region = aws.String(region)
+	config.DisableSSL = aws.Bool(disableSsl)
+	config.S3ForcePathStyle = aws.Bool(s3ForcePathStyle)
+
+	path := url.Path
+	if path != "" {
+		parts := strings.Split(strings.TrimLeft(path, "/"), "/")
+		bucket = parts[0]
+		key = strings.Join(parts[1:], "/")
+	}
+
+	return
+}
+
+func MustParse(dsn string, logger ...utils.Logger) (config *aws.Config, bucket string, key string) {
+	log := utils.EnsureLogger(logger...)
+
+	config, bucket, key, err := Parse(dsn)
+	if err != nil {
+		log.Fatalf("failed to parse s3 dsn: %v", err)
+	}
+	return config, bucket, key
+}
+
+func ParseS3(dsn string) (config *aws.Config, err error) {
+	config, bucket, _, err := Parse(dsn)
+	if bucket != "" {
+		return nil, fmt.Errorf("invalid s3 dsn: unexpected bucket: %s", bucket)
+	}
+
+	return
+}
+
+func MustParseS3(dsn string, logger ...utils.Logger) *aws.Config {
+	log := utils.EnsureLogger(logger...)
+
+	config, err := ParseS3(dsn)
+	if err != nil {
+		log.Fatalf("failed to parse s3 dsn: %v", err)
+	}
+	return config
+}
+
+func ParseS3Bucket(dsn string) (config *aws.Config, bucket string, err error) {
+	config, bucket, key, err := Parse(dsn)
+
+	if bucket == "" {
+		return nil, "", fmt.Errorf("invalid s3 bucket dsn: missing bucket")
+	}
+
+	if key != "" {
+		return nil, "", fmt.Errorf("invalid s3 bucket dsn: unexpected key: %s", key)
+	}
+
+	return
+}
+
+func MustParseS3Bucket(dsn string, logger ...utils.Logger) (*aws.Config, string) {
+	log := utils.EnsureLogger(logger...)
+
+	config, bucket, err := ParseS3Bucket(dsn)
+	if err != nil {
+		log.Fatalf("failed to parse s3 bucket dsn: %v", err)
+	}
+	return config, bucket
+}
+
+func ParseS3Object(dsn string) (config *aws.Config, bucket string, key string, err error) {
+	config, bucket, key, err = Parse(dsn)
+	if bucket == "" {
+		return nil, "", "", fmt.Errorf("invalid s3 object dsn: missing bucket")
+	}
+	if key == "" {
+		return nil, "", "", fmt.Errorf("invalid s3 object dsn: missing key")
+	}
+	return
+}
+
+func MustParseS3Object(dsn string, logger ...utils.Logger) (config *aws.Config, bucket string, key string) {
+	log := utils.EnsureLogger(logger...)
+
+	config, bucket, key, err := ParseS3Object(dsn)
+	if err != nil {
+		log.Fatalf("failed to parse s3 object dsn: %v", err)
+	}
+	return config, bucket, key
+}
---
+++ s3_dsn/s3_test.go
@@ -0,0 +1,96 @@
+package s3_dsn_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/geektheripper/vast-dsn/s3_dsn"
+)
+
+func TestS3DSNParser(t *testing.T) {
+	t.Run("amazon s3", func(t *testing.T) {
+		config, err := s3_dsn.ParseS3("s3://access_key:secret_key@-?region=us-east-2")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		cred, err := config.Credentials.Get()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if cred.AccessKeyID != "access_key" {
+			t.Error("access key not parsed")
+			return
+		}
+
+		if *config.Region != "us-east-2" {
+			t.Error("region not parsed")
+			return
+		}
+	})
+
+	t.Run("self sign https minio", func(t *testing.T) {
+		config, err := s3_dsn.ParseS3("s3://access_key:secret_key@maggie.minio.geektr.co:9003?region=")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		cred, err := config.Credentials.Get()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if cred.AccessKeyID != "access_key" {
+			t.Error("access key not parsed")
+			return
+		}
+
+		if *config.Region != "" {
+			t.Error("region not parsed")
+			return
+		}
+
+		if *config.Endpoint != "https://maggie.minio.geektr.co:9003" {
+			t.Error("endpoint not parsed")
+			return
+		}
+	})
+
+	t.Run("s3 bucket dsn", func(t *testing.T) {
+		config, bucket, err := s3_dsn.ParseS3Bucket("s3://access_key:secret_key@maggie.minio.geektr.co:9003/foobar/path/to/key?region=")
+		if bucket != "" || config != nil {
+			t.Error("wrong bucket dsn parsed")
+			return
+		}
+
+		if fmt.Sprint(err) != fmt.Sprintf("invalid s3 bucket dsn: unexpected key: %s", "path/to/key") {
+			t.Error(err)
+			return
+		}
+
+		_, bucket2, _ := s3_dsn.ParseS3Bucket("s3://access_key:secret_key@maggie.minio.geektr.co:9003/foobar?region=")
+
+		if bucket2 != "foobar" {
+			t.Error("bucket not parsed")
+			return
+		}
+	})
+
+	t.Run("s3 object dsn", func(t *testing.T) {
+		_, _, key, err := s3_dsn.ParseS3Object("s3://access_key:secret_key@maggie.minio.geektr.co:9003/foobar/path/to/key?region=")
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if key != "path/to/key" {
+			t.Error("key not parsed")
+			return
+		}
+	})
+}
